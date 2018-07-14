159da6e35137b5e10948eb3bac747c3a226d1096
RumbleFrog <xxlosttechxx@gmail.com>
2018-07-14 00:27:33 -0400

    Deployment

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,11 @@ import Home from '@/components/Home';
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-  ],
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+        },
+    ],
 });
